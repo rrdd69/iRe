9e18d9f791a6e189ff2049b1b452ee8557736a69
yglee <yglee@ksmartpia.com>
2020-12-24 08:14:56 +0900

    firstCommit

---
+++ src/test/java/cms/bbs/v1/V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cms.bbs.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
