93c9f4ba329caacba192a49da4e71c0a8911ff93
lyg0114 <whdnseowkd@naver.com>
2020-12-26 23:59:39 +0900

    DATABASE 연결및 JPA 연동

---
+++ src/main/java/cms/bbs/v1/entity/Memo.java
@@ -0,0 +1,24 @@
+package cms.bbs.v1.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="TBL_MEMO")
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
+
+}
