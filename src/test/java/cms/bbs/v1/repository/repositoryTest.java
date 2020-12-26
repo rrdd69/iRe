eaa783e39064e3e1e8ee911e082a4967df038870
lyg0114 <whdnseowkd@naver.com>
2020-12-27 07:54:15 +0900

    CRUD TEST Code

--- src/test/java/cms/bbs/v1/repository/repositoryTest.java
+++ src/test/java/cms/bbs/v1/repository/repositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,13 +26,41 @@ class MemoRepositoryTest {
     }
 
     @Test
-    public void 더미데이터_입력(){
+    public void 더미데이터_입력_테스트(){
         IntStream.rangeClosed(1,100).forEach(i -> {
             Memo memo = Memo.builder().memoText("Sample..."+i).build();
             memoRepository.save(memo);
         });
     }
 
-    
+    @Test
+    public void 단건조회_테스트(){
+
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+        System.out.println("==================================");
+        if(result.isPresent()){
+            Memo memo = result.get();
+            System.out.println("memo = " + memo);
+        }
+    }
+
+
+    @Test
+    public void 수정작업_테스트(){
+        Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+        System.out.println(memoRepository.save(memo));
+
+    }
+
+    @Test
+    public void 삭제작업_테스트(){
+
+        memoRepository.deleteById(100L);
+
+    }
+
+
 
 }
