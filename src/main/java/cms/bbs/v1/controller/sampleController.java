fa60a262f053caa22e09fb8535d5c96371723f15
lyg0114 <whdnseowkd@naver.com>
2020-12-27 21:47:49 +0900

    Sample Controller

--- src/main/java/cms/bbs/v1/controller/sampleController.java
+++ src/main/java/cms/bbs/v1/controller/sampleController.java
@@ -1,14 +1,19 @@
 package cms.bbs.v1.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/sample")
+@Log4j2
 public class sampleController {
 
     @GetMapping("/hello")
-    public String[] hello(){
-        return new String[]{"hello", "world"};
+    public void hello(){
+        log.info("##################################");
     }
 
 }
