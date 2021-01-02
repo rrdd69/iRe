0aa696afba17da29b9daa12ca0fd2ae553d9e51c
lyg0114 <whdnseowkd@naver.com>
2021-01-03 07:55:47 +0900

    QueryDSL 다양한 쿼리 생성

--- src/test/java/cms/bbs/v1/repository/GuestbookRepositoryTests.java
+++ src/test/java/cms/bbs/v1/repository/GuestbookRepositoryTests.java
@@ -51,8 +51,10 @@ public class GuestbookRepositoryTests {
 
 
     }
+
+
     @Test
-    public void testQuery1(){
+    public void testSearchQuery(){
         Pageable pageable = PageRequest.of(0,10, Sort.by("gno").descending());
 
         QGuestbook qGuestbook = QGuestbook.guestbook;
@@ -64,6 +66,36 @@ public class GuestbookRepositoryTests {
         BooleanExpression expression = qGuestbook.title.contains(keyword);
 
         builder.and(expression);
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println("guestbook = " + guestbook);
+        });
+
+    }
+
+    @Test
+    public void testMultiSearchQuery(){
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = "2";
+        String user = "";
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+        BooleanExpression exUser = qGuestbook.writer.contains(user);
+        BooleanExpression exAll = exTitle.or(exContent).and(exUser);
+
+        builder.and(exAll);
+        builder.and(qGuestbook.gno.gt(0L));
+        builder.and(qGuestbook.gno.lt(200L));
+
 
         Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
 
