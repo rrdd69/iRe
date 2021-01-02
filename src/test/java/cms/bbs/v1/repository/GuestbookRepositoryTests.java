4a1751b8b616fd6ec6fc92bb92eff1f09ded0f5e
lyg0114 <whdnseowkd@naver.com>
2021-01-03 07:26:54 +0900

    QueryDSL 기본 테스트 코드

--- src/test/java/cms/bbs/v1/repository/GuestbookRepositoryTests.java
+++ src/test/java/cms/bbs/v1/repository/GuestbookRepositoryTests.java
@@ -1,9 +1,17 @@
 package cms.bbs.v1.repository;
 
 import cms.bbs.v1.entity.Guestbook;
+import cms.bbs.v1.entity.QGuestbook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -40,6 +48,30 @@ public class GuestbookRepositoryTests {
             guestbook.changeContent("Change Content");
             guestbookRepository.save(guestbook);
         }
+
+
+    }
+    @Test
+    public void testQuery1(){
+        Pageable pageable = PageRequest.of(0,10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+
+        builder.and(expression);
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println("guestbook = " + guestbook);
+        });
+
+
 
 
     }
