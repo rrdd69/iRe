74b370338aec9aaadd9c8a58f62560dd6bbf345d
lyg0114 <whdnseowkd@naver.com>
2021-01-05 00:11:11 +0900

    list 화면

--- src/main/java/cms/bbs/v1/controller/GuestBookController.java
+++ src/main/java/cms/bbs/v1/controller/GuestBookController.java
@@ -1,22 +1,32 @@
 package cms.bbs.v1.controller;
 
+import cms.bbs.v1.dto.PageRequestDTO;
+import cms.bbs.v1.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor // 자동 주입을 위한 Anotation
 public class GuestBookController {
 
-    @GetMapping({"/","/list"})
-    public String list(){
+    private final GuestbookService service;
 
-        log.info("################################");
-        log.info("LIST");
-        log.info("################################");
+    @GetMapping({"/"})
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
 
-        return "guestbook/list";
+    @GetMapping({"/list"})
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("#############################"+pageRequestDTO);
+        model.addAttribute("result", service.getLsit(pageRequestDTO));
+
     }
 }
