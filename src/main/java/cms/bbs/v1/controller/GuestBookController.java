19b07e87335476fbfd212db52035e045349d6879
lyg0114 <whdnseowkd@naver.com>
2021-01-06 08:30:47 +0900

    등록기능

--- src/main/java/cms/bbs/v1/controller/GuestBookController.java
+++ src/main/java/cms/bbs/v1/controller/GuestBookController.java
@@ -1,5 +1,6 @@
 package cms.bbs.v1.controller;
 
+import cms.bbs.v1.dto.GuestbookDTO;
 import cms.bbs.v1.dto.PageRequestDTO;
 import cms.bbs.v1.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +9,9 @@ import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -28,4 +31,24 @@ public class GuestBookController {
         log.info("#############################"+pageRequestDTO);
         model.addAttribute("result", service.getLsit(pageRequestDTO));
     }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto..."+ dto);
+
+        //새로 추가된 엔티티의 번호
+        Long gno = service.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
+    }
+
+
+
+
 }
